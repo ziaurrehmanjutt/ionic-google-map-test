281b63fdd410a91212d335ea1c49f1ccc5b8bbb7
Zia <ziaa520@gmail.com>
2022-01-15 23:01:12 +0500

    Upload

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,10 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.zia.maptest',
-  appName: 'mapTest',
-  webDir: 'www',
-  bundledWebRuntime: false
+  
 };
 
 export default config;
